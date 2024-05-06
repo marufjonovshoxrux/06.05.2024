76d33986bea6758a39065b5968d1328272b378fd
Shoxrux Marufjonov <shokha0502@gmail.com>
2024-05-06 18:45:04 +0500

    main

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// function names(...arr) {
+//     let max = arr 
+
+//     for(let name of arr) {
+//         if (name.length > max.length) {
+//             max = name
+            
+//         }
+
+//     }
+//     return max
+// }
+
+// let maxName = names('Farzod','Samir','Shoxrux','Damir')
+
+// console.log('Longest name:',maxName);
+
+
+
+
